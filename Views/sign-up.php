8175e6e11e80b06007f8b4b25a38040d326ece9a
ishikoro333 <ishikoro33_33@yahoo.co.jp>
2021-07-31 12:24:45 +0900

    ユーザー画面

--- Views/sign-up.php
+++ Views/sign-up.php
@@ -19,8 +19,8 @@
             <h1>アカウントを作る</h1>
             <input type="text" class="form-control" name="nickname" placeholder="ニックネーム" maxlength="50" required autofocus>
             <input type="text" class="form-control" name="name" placeholder="ユーザー名、例)taro" maxlength="50" required>
-            <input type="email" class="form-control" name="email" placeholder="メールアドレス" maxlength="254" required autofocus>
-            <input type="password" class="form-control" name="password" placeholder="パスワード" minlength="4" maxlength="50" required autofocus>
+            <input type="email" class="form-control" name="email" placeholder="メールアドレス" maxlength="254" required>
+            <input type="password" class="form-control" name="password" placeholder="パスワード" minlength="4" maxlength="128" required>
             <button class="w-100 btn btn-lg" type="submit">登録する</button><!-- btn-lg/w-100はbsのクラス-->
             <p class="mt-3 mb-2"><a href="sign-in.php">ログインする</a></p>
             <p class="mt-2 mb-3 text-muted">&copy; 2021</p>
