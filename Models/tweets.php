526154ad9248277cb0cec4de80be9e1fe1d9f3cc
ishikoro333 <ishikoro33_33@yahoo.co.jp>
2021-08-04 16:47:00 +0900

    tweet投稿機能

---
+++ Models/tweets.php
@@ -0,0 +1,101 @@
+<?php
+///////////////////////
+//ツイートデータを処理
+///////////////////////
+
+/**
+ * ユーザーを作成
+ * 
+ * @param array $data
+ * @return bool  
+ */
+
+ function createTweet(array $data){
+     //DB接続
+    $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+     //接続エラーがある場合->処理を停止
+     if($mysqli->connect_errno){
+        echo 'MySQLの接続に失敗しました。:'  . $mysqli->connect_error . "\n";
+        exit;
+     }
+
+     //新規登録のsqlクエリを作成
+     $query = 'INSERT INTO tweets (user_id, body, image_name) VALUES (?, ?, ?)';
+
+     //プリペアドステートメントに、作成したクエリを登録
+     $statement = $mysqli->prepare($query);
+    
+     //クエリのプレースホルダ($queryの?の部分)にカラム値を紐付け iss 左からint型、string型の順
+     $statement->bind_param('iss',$data['user_id'],$data['body'],$data['image_name']);
+
+     //クエリを実行
+     $response = $statement->execute();
+
+     //実行に失敗した場合->エラー表示
+     if($response === false){
+         echo 'エラーメッセージ:' . $mysqli->error ."\n";
+     }
+    
+     //DB接続を開放
+     $statement->close();
+     $mysqli->close();
+
+     return $response;
+ }
+
+/**
+ * ユーザー情報取得:ログインチェック
+ * 
+ * @param string $email
+ * @param string $password
+ * @return array|false
+ */
+function findUserAndCheckPassword(string $email, string $password)
+{
+    //DB接続
+    $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+    //接続エラーがある場合->処理停止
+    if ($mysqli->connect_errno){
+        echo 'MySQLの接続に失敗しました。: ' .$mysqli -> connect_error ."\n";
+        exit;
+    }
+
+    //入力値をエスケープ
+    $email = $mysqli->real_escape_string($email);
+    
+    //sqlクエリを作成
+    // - 外部からのリクエストは何が入ってくるかわからないため、必ず、エスケープしたものをクオートで囲む
+    $query = 'SELECT * FROM users WHERE email = "'. $email . '"';
+
+    //クエリを実行
+    $result = $mysqli->query($query);
+
+    //クエリ実行に失敗した場合->return
+    if(!$result){
+        //MySQL処理中にエラー発生
+        echo 'エラーメッセージ:' . $mysqli->error . "\n";
+        $mysqli->close();
+        return false;
+    }
+
+    //ユーザー情報を取得
+    $user  = $result->fetch_array(MYSQLI_ASSOC);
+    //ユーザーが存在しない場合->return
+    if(!$user){
+        $mysqli->close();
+        return false;
+    }
+
+    //パスワードチェック、不一致の場合->return
+    if(!password_verify($password, $user['password'])){
+        $mysqli->close();
+        return false;
+    }
+
+    //DBを開放
+    $mysqli->close();
+
+    return $user;
+}
--- util.php
+++ util.php
@@ -121,3 +121,41 @@ function getUserSession()
 
     return $user;
 }
+
+/**
+ * 画像をアップロード
+ * 
+ * @param array $user
+ * @param array $file
+ * @param string $type
+ * @return string 画像のファイル名
+ */
+
+
+function uploadImage(array $user, array $file, string $type) //$typeには'user'or'tweet'文字列が入る
+{
+    //画像のファイル名から拡張子を取得
+    $image_extension = strrchr($file['name'],'.');
+
+    //画像のファイル名を作成(YmdHis: 2021-01-01 00:00:00 ならば 20210101000000)
+    $image_name = $user['id'] . '_' . date('YmdHis') . $image_extension;
+
+    //保存先のディレクトリ
+    $directory = '../Views/img_uploaded/' . $type . '/';
+
+    //画像のパス
+    $image_path =  $directory . $image_name;
+    
+    //画像を設置
+    move_uploaded_file($file['tmp_name'], $image_path);
+    //アップロードされた第1引数の一時ファイルを、第2引数の指定箇所に移動
+    
+    //画像ファイルの場合 -> ファイル名をreturn
+    if (exif_imagetype($image_path)){
+        return $image_name;
+    }
+
+    //画像ファイル以外の場合
+    echo '選択されたファイルが画像ではないため処理を停止しました';
+    exit;
+}
